174a6311317b5c0d11f5512470af5d9dc5430b3f
Uberto Barbini <uberto.barbino@springernature.com>
2019-02-16 21:53:40 +0000

    adding lambdas with receiver examples

---
+++ src/main/kotlin/com/gamasoft/functionLiteralsWithReceiver/DbSession.kt
@@ -0,0 +1,38 @@
+package com.gamasoft.functionLiteralsWithReceiver
+
+
+data class User(val id: Int, val name: String)
+
+interface UserRepository{
+    fun getUser(id: Int): User
+
+    fun getAllUsers(): List<User>
+
+    fun saveUser(user: User)
+
+}
+
+class DbSession(val dbConn: String, val block: (UserRepository) -> Unit){
+
+    fun execute(){
+        val userRepo = UserDb(dbConn)
+        userRepo.open()
+
+        block(userRepo)
+
+        userRepo.close()
+    }
+}
+
+class UserDb(dbConn: String) : UserRepository {
+    override fun getUser(id: Int): User {}
+
+    override fun getAllUsers(): List<User> {}
+
+    override fun saveUser(user: User) {}
+
+    fun open(){}
+
+    fun close(){}
+
+}
---
+++ src/main/kotlin/com/gamasoft/propertyDelegation/DelegatedClass.kt
@@ -0,0 +1,22 @@
+package com.gamasoft.propertyDelegation
+
+import kotlin.reflect.KProperty
+
+class DelegatedClass {
+    val a: String by Delegate()
+    val b: String by Delegate()
+
+    override fun toString() = "Delegated Class"
+}
+
+class Delegate() {
+    operator fun getValue(thisRef: Any?, prop: KProperty<*>): String =
+        "$thisRef, is '${prop.name}' ${prop.returnType}"
+
+}
+
+fun main() {
+    val e = DelegatedClass()
+    println(e.a)
+    println(e.b)
+}
