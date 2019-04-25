6ba8ac05b069cafddb48f75b9cda0120059f6bad
uberto <uberto.gama@gmail.com>
2019-04-25 22:17:40 +0100

    code tidy up

--- src/main/kotlin/com/ubertob/implementationDelegation/DbPersistence.kt
+++ src/main/kotlin/com/ubertob/implementationDelegation/DbPersistence.kt
@@ -3,26 +3,15 @@ package com.ubertob.implementationDelegation
 import com.ubertob.functionLiteralsWithReceiver.User
 
 
-interface UserPersistence{
-    //what is needed by the domain
-    fun fetchUser(userId: Int): User
-    fun fetchAll(): List<User>
-}
-
-class UserPersistenceBySql(dbConn: DbConnection): UserPersistence, SqlRunner<User> by UserSql(dbConn) {
-   //translate domain in sql statements but still abstract from db connection,transactions etc.
-
-    override fun fetchUser(userId: Int): User {
-        return fetchSingle("select * from users where id = $userId")
-    }
-
-    override fun fetchAll(): List<User> {
-        return fetchMulti("select * from users")
-    }
-}
 
 
 typealias Row = Map<String, Any> //a db row is expressed as a Map field->value
+
+interface DbConnection {
+    //abstraction on the db connection/transaction etc
+    fun executeQuery(sql: String): List<Row>
+}
+
 
 interface SqlRunner<out T> { //out is because we can return T or subtypes
     // interface to execute sql statement and return domain objects
@@ -35,6 +24,38 @@ fun <T> SqlRunner<T>.fetchSingle(sql: String): T = builder( executeQuery(sql).fi
 fun <T> SqlRunner<T>.fetchMulti(sql: String): List<T> = executeQuery(sql).map { builder(it) }
 
 
+//real example would be: class JdbcDbConnection(dbConnString: String): DbConnection
+
+class FakeDbConnection(): DbConnection{
+    //trivial example but in reality manage connection pool, transactions etc and translate from JDBC
+    override fun executeQuery(sql: String): List<Row> {
+        return listOf( mapOf("id" to 5, "name" to "Joe") )
+    }
+
+}
+
+
+
+
+//now how to use all this for retrieve Users
+
+interface UserPersistence{
+    //interface needed by the domain
+    fun fetchUser(userId: Int): User
+    fun fetchAll(): List<User>
+}
+
+class UserPersistenceBySql(dbConn: DbConnection): UserPersistence, SqlRunner<User> by UserSql(dbConn) {
+    //translate domain in sql statements but still abstract from db connection,transactions etc.
+
+    override fun fetchUser(userId: Int): User {
+        return fetchSingle("select * from users where id = $userId")
+    }
+
+    override fun fetchAll(): List<User> {
+        return fetchMulti("select * from users")
+    }
+}
 
 class UserSql( dbConn: DbConnection) : SqlRunner<User>, DbConnection by dbConn {
     // implementation for User
@@ -43,26 +64,8 @@ class UserSql( dbConn: DbConnection) : SqlRunner<User>, DbConnection by dbConn {
         name = row["name"] as String)
 
     // note that we don't need to implement executeQuery because is already in DbConnection
-
 }
 
-
-
-interface DbConnection {
-    //abstraction on the db connection/transaction etc
-    fun executeQuery(sql: String): List<Row>
-}
-
-
-//real example would be: class JdbcDbConnection(dbConnString: String): DbConnection
-
-class FakeDbConnection(): DbConnection{
-//trivial example but in reality manage connection pool, transactions etc and translate from JDBC
-    override fun executeQuery(sql: String): List<Row> {
-        return listOf( mapOf("id" to 5, "name" to "Joe") )
-    }
-
-}
 
 
 object UserRepository: UserPersistence by UserPersistenceBySql(FakeDbConnection())
--- src/main/kotlin/com/ubertob/implementationDelegation/MultipleInheritance.kt
+++ src/main/kotlin/com/ubertob/implementationDelegation/MultipleInheritance.kt
@@ -5,34 +5,52 @@ package com.ubertob.implementationDelegation
 data class Image (val name: String)
 
 interface Printer{
-    fun switchOn()
-    fun switchOff()
-    fun print(image: Image)
+    fun turnOn()
+    fun isOn(): Boolean
+    fun printCopy(image: Image)
 }
 
 interface Scanner{
-    fun switchOn()
-    fun switchOff()
+    fun turnOn()
+    fun isOn(): Boolean
     fun scan(name: String): Image
 }
 
 object laserPrinter: Printer {
     var working = false
-    override fun switchOn() {
+
+    override fun isOn(): Boolean = working
+
+    override fun turnOn() {
         working = true
     }
 
-    override fun switchOff() {
-        working = false
-    }
-
-    override fun print(image: Image) {
+    override fun printCopy(image: Image) {
         println("printed ${image.name}")
     }
 
 }
 
+object fastScanner: Scanner {
+    var working = false
 
-class scannerAndPrinter: Scanner, Printer by laserPrinter {
+    override fun isOn(): Boolean = working
+
+    override fun turnOn() {
+        working = true
+    }
+
     override fun scan(name: String): Image = Image(name)
+
 }
+
+class ScannerAndPrinter(scanner: Scanner, printer: Printer): Scanner by scanner, Printer by printer {
+
+    override fun isOn(): Boolean = (this as Scanner).isOn()
+
+    override fun turnOn() = (this as Scanner).turnOn()
+
+    fun scanAndPrint(imageName: String) = printCopy(scan(imageName))
+
+}
+
