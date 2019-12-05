680872ccb55fa4fa55be4a36d4dd963ab47a6cbc
uberto <uberto.gama@gmail.com>
2019-12-05 22:59:55 +0000

    tidy up

--- src/main/java/com/ubertob/java/MiniString.java
+++ src/main/java/com/ubertob/java/MiniString.java
@@ -2,8 +2,6 @@ package com.ubertob.java;
 
 public class MiniString {
 
-    public static final int MAX_MINISTR_LEN = 10;
-    public static final int MINI_STR_BASE = 64;
     long raw;
 
     public MiniString(String str) {
@@ -14,13 +12,12 @@ public class MiniString {
         return decode(raw);
     }
 
-    public static final char terminator = '=';
-    public static final String letters = terminator + "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 _-!?.$&%@#:[]{}()*<>:;',/^";
+    public static final int MAX_MINISTR_LEN = 10;
+    public static final int MINI_STR_BASE = 64;
+    public static final String letters = "=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 _-!?.$&%@#:[]{}()*<>:;',/^";
 
     public static long encode(String str) {
         String prepared = prepareString(str);
-
-        System.out.println("prepared " + prepared);
         long encoded = 0;
         for (char c : prepared.toCharArray()) {
             int x = letters.indexOf(c);
@@ -45,22 +42,13 @@ public class MiniString {
         long remaining = number;
 
         while (true) {
-            System.out.println("remaining " + remaining);
-
             int mod = (int) ( remaining % MINI_STR_BASE);
-
-            System.out.println("mod " + mod);
             char c = letters.charAt(mod);
-
-            System.out.println("c " + c);
-
             decoded.insert(0, c);
             if ( remaining < MINI_STR_BASE)
                 break;
             remaining = remaining / MINI_STR_BASE;
         }
-        System.out.println("decoded " + decoded);
-
         return decoded.toString();
 
     }
