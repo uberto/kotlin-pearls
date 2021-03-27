603048615804ea113ee56f998599e22e7b1d9088
uberto <uberto.gama@gmail.com>
2021-03-27 08:43:42 +0000

    playing with companion objects

--- src/main/kotlin/com/ubertob/companionObjects/CompanionedClass.kt
+++ src/main/kotlin/com/ubertob/companionObjects/CompanionedClass.kt
@@ -2,26 +2,32 @@ package com.ubertob.companionObjects
 
 import kotlin.math.sqrt
 
-class CompanionedClass(val x: Double, val y: Double) {
+interface IShape {
+
+    companion object {
+        fun doSomething() {
+            println("!!!")}
+    }
+}
+
+class CompanionedClass private constructor(val x: Double, val y: Double) {
 
     fun distance(): Double = sqrt(x*x + y*y)
-
-
 
 
 //    companion object {
 //        fun shape(l: Double): CompanionedClass = CompanionedClass(l, l)
 //    }
 
-    companion object Square {
+    companion object Square: IShape {
         fun shape(l: Double): CompanionedClass = CompanionedClass(l, l)
     }
 
-    object HRect {
+    object HRect:IShape {
         fun shape(l: Double): CompanionedClass = CompanionedClass(l * 2, l)
     }
 
-    object VRect {
+    object VRect:IShape {
         fun shape(l: Double): CompanionedClass = CompanionedClass(l, l * 2)
     }
 
@@ -29,9 +35,14 @@ class CompanionedClass(val x: Double, val y: Double) {
 
 
 fun main(){
+    val myShape = CompanionedClass.shape(5.0)
+//    myShape.shape(4.5) don't work
 
     CompanionedClass.shape(5.0)
     CompanionedClass.Square.shape(5.0)
     CompanionedClass.HRect.shape(5.0)
     CompanionedClass.VRect.shape(5.0)
+    val vRect: IShape = CompanionedClass.VRect
+//    vRect.doSomething()
+    IShape.doSomething()
 }
--- src/main/kotlin/com/ubertob/companionObjects/InterfaceCompanion.kt
+++ src/main/kotlin/com/ubertob/companionObjects/InterfaceCompanion.kt
@@ -31,19 +31,27 @@ data class MyAutoBuild(val raw: String): IBuilder<String, MyAutoBuild>{
 
 
 interface I1 : (String) -> Boolean {
-    companion object
+    companion object {
+        fun isCiao(): I1 = object: I1{
+            override fun invoke(p1: String): Boolean = p1 == "ciao"
+        }
+    }
 }
 
 
-fun interface I2 : (String) -> Boolean {
-//    companion object
-}
+fun interface I2 : (String) -> Boolean
+
 
 fun main() {
 
-//    val a: I1.Companion = I1
+    val a: I1.Companion = I1
+    val b = a.isCiao()
+    println(b("ciaociao"))
+
 //    val b: I2 =   I2  //doesn't compile
-    val c: I2 =   I2 { false }
+    val strEvenLen: I2 =   I2 { (it.length % 2) == 0 }
+
+    println(strEvenLen("ciaociao"))
 
     val tt = MyAutoBuild("ciccio")
 
